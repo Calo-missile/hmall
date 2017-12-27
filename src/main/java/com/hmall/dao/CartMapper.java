e15481494ba47fa065144fdbd7b6958bc73ac856
Calo-missile <15827406169@163.com>
2017-12-27 23:27:31 +0800

    基本框架

--- src/main/java/com/hmall/dao/CartMapper.java
+++ src/main/java/com/hmall/dao/CartMapper.java
@@ -2,16 +2,18 @@ package com.hmall.dao;
 
 import com.hmall.pojo.Cart;
 
-public interface CartMapper {
-    int deleteByPrimaryKey(Integer id);
+public interface CartMapper{
+    int deleteByPrimaryKey(Integer id);//根据主键去删除
 
-    int insert(Cart record);
+    int insert(Cart record);//插入购物车   将对象完全插入表中
 
-    int insertSelective(Cart record);
+    int insertSelective(Cart record);//根据选举进行插入
 
-    Cart selectByPrimaryKey(Integer id);
+    Cart selectByPrimaryKey(Integer id);//根据主键去查询
 
     int updateByPrimaryKeySelective(Cart record);
 
     int updateByPrimaryKey(Cart record);
+
+
 }
